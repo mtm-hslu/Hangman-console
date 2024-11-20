028862da8cbb810a3e4386202e47a553d6ff7dad
Mouhamadou Thiam <mouhamadou.thiam@stud.hslu.ch>
2024-11-20 13:24:48 +0100

    code skeleton created

---
+++ hangman.py
@@ -0,0 +1,77 @@
+import os
+
+def display_hangman(tries):
+    stages = [
+        """
+           -----
+           |   |
+           O   |
+          /|\\  |
+          / \\  |
+               |
+        =========
+        """,
+        """
+           -----
+           |   |
+           O   |
+          /|\\  |
+          /    |
+               |
+        =========
+        """,
+        """
+           -----
+           |   |
+           O   |
+          /|\\  |
+               |
+               |
+        =========
+        """,
+        """
+           -----
+           |   |
+           O   |
+          /|   |
+               |
+               |
+        =========
+        """,
+        """
+           -----
+           |   |
+           O   |
+           |   |
+               |
+               |
+        =========
+        """,
+        """
+           -----
+           |   |
+           O   |
+               |
+               |
+               |
+        =========
+        """,
+        """
+           -----
+           |   |
+               |
+               |
+               |
+               |
+        =========
+        """
+    ]
+    return stages[tries]
+
+def hangman(word):
+    print("Welcome to Hangman!")
+    # logic will be here
+
+if __name__ == "__main__":
+    word_to_guess = input("Enter the word to guess: ").strip()
+    hangman(word_to_guess)
